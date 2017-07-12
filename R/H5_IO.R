096bb10787106783258f2481230d121522127759
mikejiang <wjiang2@fhcrc.org>
2017-07-12 10:44:36 -0700

    add H5transpose function to convert cell-wise mat to gene-wise more efficiently

--- R/H5_IO.R
+++ R/H5_IO.R
@@ -19,7 +19,7 @@ h5read.chunked <- function(h5, name, index, ncol, verbose = FALSE, ...){
     sub <- h5read1(h5, name, j)
     # browser()
     if(!is.null(rind))
-      sub <- sub[rind, ]
+      sub <- sub[rind,, drop = FALSE ]
     sub
   })
   do.call(cbind,res)
@@ -53,5 +53,54 @@ H5write.blocks <- function(mat, h5file, ncol, nrow, compress = c("lz4", "gzip", 
     h5write1(as.matrix(mat[,j, drop = F]), h5file, "data", colIndx = j)
   }
     
+  
+}
+
+#' transpose a 2d data mattrix in H5
+#' @param src the H5 file that contains the 'data' mattrix to be transposed
+#' @param dest the H5 file name to be written to
+#' @export
+#' @importFrom rhdf5 H5Fopen H5Dopen H5Dget_space H5Sget_simple_extent_dims H5close
+H5transpose <- function(src, dest, compress = c("lz4", "gzip", "none"), nLevel = NULL, verbose = FALSE, ...){
+  
+  fid <- H5Fopen(src)
+  ds <- H5Dopen(fid, "data")
+  spc <- H5Dget_space(ds)
+  dims <- H5Sget_simple_extent_dims(spc)
+  dims <- dims[["size"]]
+  H5close()
+  dims <- rev(dims) #transpose it
+  ncol <- dims[2] 
+  nrow <- dims[1]
+  compress <- match.arg(compress)
+  if(file.exists(dest))
+    file.remove(dest)
+  
+  h5createFile(dest)
+  if(is.null(nLevel))
+    nLevel <- ifelse(compress == "lz4", -1, 6)
+  
+  h5createDataset1(dest, "data", rev(c(nrow, ncol)), storage_mode = "double"
+                   , chunk_dims = rev(c(nrow, 1))#note that H5 dims start with col
+                   , compressor = match(compress, c("lz4", "gzip", "none"))
+                   , nLevel = nLevel)
+  grps <- split.block(seq_len(ncol), ...)
+  #write subset of columns (avoid coerce entire mat into memory)
+  for(i in seq_along(grps))
+  {
+    
+    j <- grps[[i]]
+    if(verbose)
+      cat("group:", i, " out of ", length(grps), "\n")
+    #read rows from src
+    if(verbose)
+      cat("reading the source..")
+    mat <- h5read.chunked(src, "data", list(j, NULL), nrow, ...)
+    #write them as cols to dest
+    if(verbose)
+      cat("writing..\n")
+    h5write1(t(mat), dest, "data", colIndx = j)
+  }
+  
   
 }
